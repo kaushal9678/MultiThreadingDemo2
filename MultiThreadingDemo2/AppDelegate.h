72d26f8debb8facac12f26d2ab5e919375117b62
kaushal9678 <k.kaushal50@gmail.com>
2015-08-05 14:57:04 +0530

    Initial Commit

---
+++ MultiThreadingDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThreadingDemo2
+//
+//  Created by mac on 8/5/15.
+//  Copyright (c) 2015 vstacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiThreadingDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiThreadingDemo2
+//
+//  Created by mac on 8/5/15.
+//  Copyright (c) 2015 vstacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
